cd0eadf01f9bd38e5ab39527a78ca8df37c0d44f
vijaykumaryenne <vijaykumar.yenne@bitsaa.org>
2026-01-03 19:29:27 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     basic.showLeds(`
         . . . . .
         . . # . .
